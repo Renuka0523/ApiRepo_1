fba7fc73609ebb4538a859b6fecf1b7c56890ba6
Renuka Ajay Chougule <renukachougule.0523@gmail.com>
2024-12-11 22:08:57 +0530

    Add new project

---
+++ SortArrayDemo/Controllers/sortArray.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SortArrayDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class sortArray : ControllerBase
+    {
+        [HttpGet(Name = "getsortArry")]
+
+        public IActionResult getsortArray([FromQuery] int[] numbers)
+        {
+            return Ok("sortArry numbers:" + numbers + "and" + numbers + "and" + numbers + "and" + numbers);
+        }
+    }
+}
